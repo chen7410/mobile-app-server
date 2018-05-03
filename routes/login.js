de5158f3c575924b3588b32d4cecd7e3428c287c
Minqing <chen7410@uw.edu>
2018-05-03 13:26:01 -0700

    a

---
+++ routes/login.js
@@ -0,0 +1,54 @@
+//express is the framework we're going to use to handle requests
+const express = require('express');
+
+const FormData = require("form-data");
+
+const bodyParser = require("body-parser");
+
+
+//Create connection to Heroku Database
+let db = require('../utilities/utils').db;
+
+let getHash = require('../utilities/utils').getHash;
+
+var router = express.Router();
+//This allows parsing of the body of POST requests, that are encoded in JSON
+router.use(bodyParser.json());
+
+//app.get('/users') means accept http 'GET' requests at path '/users'
+router.post('/', (req, res) => {
+    let user = req.body['username'];
+    let theirPw = req.body['password'];
+    let wasSuccessful = false;
+    if(user && theirPw) {
+        //Using the 'one' method means that only one row should be returned
+        db.one('SELECT Password, Salt FROM Members WHERE Username=$1', [user])
+        //If successful, run function passed into .then()
+        .then(row => {
+            let salt = row['salt'];
+            let ourSaltedHash = row['password']; //Retrieve our copy of the password
+            let theirSaltedHash = getHash(theirPw, salt); //Combined their password with our salt, then hash
+            let wasCorrectPw = ourSaltedHash === theirSaltedHash; //Did our salted hash match their salted hash?
+            //Send whether they had the correct password or not
+            res.send({
+                success: wasCorrectPw,
+                // crypt: salt
+            });
+        })
+        //More than one row shouldn't be found, since table has constraint on it
+        .catch((err) => {
+            //If anything happened, it wasn't successful
+            res.send({
+                success: false,
+                message: err
+            });
+        });
+    } else {
+        res.send({
+            success: false,
+            message: 'missing credentials'
+        });
+    }
+});
+
+module.exports = router;
---
+++ utilities/sql_conn.js
@@ -0,0 +1,13 @@
+const pgp = require('pg-promise')();
+//We have to set ssl usage to true for Heroku to accept our connection
+pgp.pg.defaults.ssl = true;
+
+//Create connection to Heroku Database
+const db = pgp('postgres://qxgapceaxlelou:0bd94f61398086795cfe617f26913574bf18d212b7e211dad437b9f58a1a5080@ec2-54-235-193-34.compute-1.amazonaws.com:5432/df1fe30k28aeco');
+
+if(!db) {
+   console.log("SHAME! Follow the intructions and set your DATABASE_URL correctly");
+   process.exit(1);
+}
+
+module.exports = db;
